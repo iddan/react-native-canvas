484de7ed063fb5852c38a87f29db1b3ea5b640bd
Lochlan Wansbrough <lochie@live.com>
2015-04-09 01:32:14 -0700

    Bug fixes

--- Canvas.js
+++ Canvas.js
@@ -2,6 +2,7 @@
 
 var React = require('react-native');
 var {
+  View,
   WebView
 } = React;
 
@@ -22,7 +23,7 @@ var Canvas = React.createClass({
         <WebView
           automaticallyAdjustContentInsets={false}
           contentInset={{top: 0, right: 0, bottom: 0, left: 0}}
-          html={"<style>*{margin:0;padding:0;}canvas{position:absolute;transform:translateZ(0);}</style><canvas></canvas><script>var canvas = document.querySelector('canvas');(" + renderString + ").bind(" + contextString + ")(canvas);</script>"}
+          html={"<style>*{margin:0;padding:0;}canvas{position:absolute;transform:translateZ(0);}</style><canvas></canvas><script>var canvas = document.querySelector('canvas');(" + renderString + ").call(" + contextString + ", canvas);</script>"}
           opaque={false}
           underlayColor={'transparent'}
           style={this.props.style}
