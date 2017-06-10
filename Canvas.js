a8c34111e3832ccdbe0dcff7dd395b96aea89bc0
cyrilf <cyrilf7x@gmail.com>
2017-06-10 19:42:30 +0200

    Use ES6 and update Webview deprecated properties
    Updates based on: [React Native - Webview](https://facebook.github.io/react-native/docs/webview.html)
    
    - `html` prop is now `source`
    - use of `injectedJavaScript` prop for more readability
    - remove some undocumented props (`opaque`, `underlayColor`)
    - use a stateless component rather than a class

--- Canvas.js
+++ Canvas.js
@@ -1,36 +1,21 @@
-'use strict';
+import React from 'react'
+import { View, WebView } from 'react-native'
 
-var React = require('react-native');
-var {
-  View,
-  WebView
-} = React;
+const Canvas = ({ context, render, style }) => {
+  const contextString = JSON.stringify(context)
+  const renderString = render.toString()
+  const jsCode = `var canvas = document.querySelector("canvas");(${renderString}).call(${contextString}, canvas)`
 
-var Canvas = React.createClass({
+  return (
+    <View>
+      <WebView
+        automaticallyAdjustContentInsets={false}
+        contentInset={{top: 0, right: 0, bottom: 0, left: 0}}
+        source={{html: '<style>*{margin:0;padding:0}canvas{transform:translateZ(0)}</style><canvas></canvas>'}}
+        injectedJavaScript={jsCode}
+        style={style} />
+    </View>
+  )
+}
 
-  propTypes: {
-    context: React.PropTypes.object,
-    render: React.PropTypes.func.isRequired
-  },
-
-  render() {
-
-    var contextString = JSON.stringify(this.props.context);
-    var renderString = this.props.render.toString();
-
-    return (
-      <View>
-        <WebView
-          automaticallyAdjustContentInsets={false}
-          contentInset={{top: 0, right: 0, bottom: 0, left: 0}}
-          html={"<style>*{margin:0;padding:0;}canvas{position:absolute;transform:translateZ(0);}</style><canvas></canvas><script>var canvas = document.querySelector('canvas');(" + renderString + ").call(" + contextString + ", canvas);</script>"}
-          opaque={false}
-          underlayColor={'transparent'}
-          style={this.props.style}
-        />
-      </View>
-    );
-  }
-});
-
-module.exports = Canvas;
+export default Canvas
