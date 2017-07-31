6399c2a84d4aa3bc53307030bea3441a94768d1c
Christopher Pitt <cgpitt@gmail.com>
2017-07-31 09:41:54 +0200

    Allow base64 drawImage

--- src/Canvas.js
+++ src/Canvas.js
@@ -4,6 +4,100 @@ import defineWebViewMethods from './defineWebViewMethods';
 import defineWebViewProperties from './defineWebViewProperties';
 import CanvasRenderingContext2D from './CanvasRenderingContext2D';
 
+const html = `
+  <!doctype html>
+  <html>
+    <head>
+      <style>
+        html {
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+          width: 100%;
+          height: 100%;
+        }
+
+        body {
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+        }
+
+        canvas {
+          position: absolute;
+          top: 0;
+          left: 0;
+        }
+      </style>
+    </head>
+    <body leftmargin="0" topmargin="0" rightmargin="0" bottommargin="0">
+      <script>
+        const canvas = document.createElement('canvas');
+        document.body.appendChild(canvas);
+
+        const targets = {
+          canvas,
+          context2D: canvas.getContext('2d'),
+        };
+
+        const toMessage = result => {
+          if (result instanceof Blob) {
+            return {
+              type: 'blob',
+              payload: btoa(result),
+            };
+          }
+          return {
+            type: 'json',
+            payload: JSON.stringify(result),
+          };
+        };
+
+        document.addEventListener('message', e => {
+          const {type, payload} = JSON.parse(e.data);
+          try {
+            switch (type) {
+              case 'exec': {
+                const {target, method, args} = payload;
+
+                // let's create a special context for drawImage, which allows
+                // base64 data and urls as a source...
+
+                if (method === 'drawImage' && args.length > 0 && typeof args[0] === 'string') {
+                    const image = new Image();
+
+                    image.addEventListener('load', function() {
+                        targets[target].drawImage(image, ...args.slice(1));
+                    });
+
+                    image.addEventListener('error', function(err) {
+                      throw err;
+                    });
+
+                    image.src = args[0];
+                    break;
+                } else {
+                    const result = targets[target][method](...args);
+                    const message = toMessage(result);
+                    postMessage(JSON.stringify(message));
+                    break;
+                }
+              }
+              case 'set': {
+                const {target, key, value} = payload;
+                targets[target][key] = value;
+                break;
+              }
+            }
+          } catch (err) {
+            document.body.innerHTML = '<div class="error">' + err + '</div>';
+          }
+        });
+      </script>
+    </body>
+  </html>
+`
+
 class Bus {
   actions = [];
   messageListeners = [];
@@ -78,16 +172,16 @@ export default class Canvas extends Component {
 
   render() {
     const {width, height} = this;
+
     return (
-      <View style={{width, height, overflow: 'hidden'}}>
-        <WebView
-          ref={this.handleRef}
-          style={{width, height, backgroundColor: 'transparent'}}
-          source={require('./index.html')}
-          onMessage={this.handleMessage}
-          onLoad={this.handleLoad}
-        />
-      </View>
+      <WebView
+        ref={this.handleRef}
+        style={{width, height, overflow: 'hidden', backgroundColor: 'transparent'}}
+        source={{html, baseUrl: '/'}}
+        onMessage={this.handleMessage}
+        onLoad={this.handleLoad}
+        scrollEnabled={false}
+      />
     );
   }
 }
