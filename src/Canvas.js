583c80701dd9fc8775b2fb2acaee69cd3ae187ce
Arturs Jansons <a.jansons@gmail.com>
2018-04-16 11:43:50 +0300

    Add width, height props to the Canvas

--- src/Canvas.js
+++ src/Canvas.js
@@ -36,8 +36,12 @@ export default class Canvas extends Component {
   listeners = [];
   context2D = new CanvasRenderingContext2D(this);
 
-  constructor() {
-    super();
+  constructor(props = {}) {
+    super(props);
+    const {style = {}} = props;
+    const {width, height} = style;
+    this.width = width;
+    this.height = height;
     this.bus.pause();
   }
 
@@ -108,10 +112,10 @@ export default class Canvas extends Component {
     const {style} = this.props;
     if (Platform.OS === 'android') {
       return (
-        <View style={{width, height, overflow: 'hidden', flex: 0, ...style}}>
+        <View style={{...style, width, height}}>
           <WebView
             ref={this.handleRef}
-            style={{width, height, overflow: 'hidden', backgroundColor: 'transparent'}}
+            style={{width, height, backgroundColor: 'transparent'}}
             source={{html}}
             onMessage={this.handleMessage}
             onLoad={this.handleLoad}
@@ -125,10 +129,10 @@ export default class Canvas extends Component {
       );
     }
     return (
-      <View style={{width, height, overflow: 'hidden', flex: 0, ...style}}>
+      <View style={{ ...style, width, height}}>
         <WebView
           ref={this.handleRef}
-          style={{width, height, overflow: 'hidden', backgroundColor: 'transparent'}}
+          style={{width, height, backgroundColor: 'transparent'}}
           source={{html, baseUrl: '/'}}
           onMessage={this.handleMessage}
           onLoad={this.handleLoad}
